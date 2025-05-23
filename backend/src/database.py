2a080e6ad3a825ae800d70dfa3fef58ca3f57e89
katsuyorii <katsuyoriiii@gmail.com>
2025-05-23 11:30:04 +0300

    Include postgres docker container and create connection to database

---
+++ backend/src/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from .config import settings
+
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    echo=True, # Убрать при деплое!
+)
+
+async_session = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+class BaseModel(DeclarativeBase):
+    ''' Basic SQLAlchemy model with ID primary key field '''
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
